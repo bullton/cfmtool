5ccee7a1dcea53b0da8ff39453f213167ea767d9
Neil <yulin.xian@nokia.com>
2017-12-22 01:01:33 +0800

    add mongodb

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,45 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import *
+
 
 app = Flask(__name__)
+nav = Nav()
+
+Bootstrap(app)
+
+
 from app import views
+
+@nav.navigation()
+def mynavbar():
+    return Navbar(
+        'Customer Fault Mamagement Tool',
+        View('Home', 'index'),
+        Subgroup('File',
+                View('Export','index'),
+        ),
+        Subgroup('Rule',
+                View('New Rule','index'),
+                View('Edit Rule','index'),
+                View('Delete Rule','index'),
+        ),
+        Subgroup('Static',
+                View('System Module/Severity Overview','index'),
+                View('Category Overview','index'),
+                View('Dedicated Test Finding','index'),
+                View('TOP Pronto','index'),
+                View('TOP Bloker','index'),
+        ),
+        Subgroup('Trend',
+                View('Customer Pronto Trend','index'),
+                View('Pronto Trend','index'),
+                View('Top Pronto Trend','index'),
+        ),
+        View('Help', 'index'),
+    )
+
+# ...
+
+nav.init_app(app)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,34 @@
+from flask import render_template
+from flask_pymongo import PyMongo
 from app import app
+
+app.config.update(
+    MONGO_HOST='127.0.0.1',
+    MONGO_PORT=27017,
+    MONGO_USERNAME='dba',
+    MONGO_PASSWORD='dba',
+    MONGO_DBNAME='mydb'
+)
+mongo = PyMongo(app)
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = { 'nickname': 'Miguel' } # fake user
+    posts = [ # fake array of posts
+        {
+            'author': { 'nickname': 'John' },
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': { 'nickname': 'Susan' },
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    books = mongo.db.books.find_one({'lang': 'python'})
+    return render_template("index.html",
+        title = 'Customer Fault Mamagement Tool',
+        user = user,
+        posts = posts,
+        name = books['author'])
